f91fd3af04ca9cadf25a0463de420712f51a91e9
Saravanan R <saravananesec@gmail.com>
2021-11-25 14:46:58 +0530

    Function Added

--- StartUpGitHub/ViewController.swift
+++ StartUpGitHub/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.displayUploadImg()
     }
-
+    
+    func displayUploadImg() {
+        print("Upload Img")
+    }
 
 }
 
